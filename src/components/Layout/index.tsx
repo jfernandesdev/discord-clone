019b022940ad34a4ab1778a4f6255dfd2ec402cd
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-11 11:31:20 -0300

    creating the parent component layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Grid } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <Grid>
+      <div>ServerList</div>
+      <div>ServerName</div>
+      <div>ChannelInfo</div>
+    </Grid>
+  );
+}
+
+export default Layout;
